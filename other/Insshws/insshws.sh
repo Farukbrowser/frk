58343efc2e8fbd650bcb037282043ed2ea6e5f22
Farukbrowser <40906618+Farukbrowser@users.noreply.github.com>
2023-05-22 07:24:04 +0100

    Update insshws.sh

--- other/Insshws/insshws.sh
+++ other/Insshws/insshws.sh
@@ -6,9 +6,9 @@ apt install python -y
 
 #Install Script Websocket-SSH Python
 wget -O /usr/local/bin/edu-proxy https://raw.githubusercontent.com/Farukbrowser/frk/main/all/websocket/https.py && chmod +x /usr/local/bin/edu-proxy
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Farukbrowser/frk/main/Insshws/dropbear-ws.py.txt
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Farukbrowser/frk/main/Insshws/ws-stunnel.txt
-wget -O /usr/local/bin/edu-proxyovpn https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/ovpn.py && chmod +x /usr/local/bin/edu-proxyovpn
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/Farukbrowser/frk/main/insshws/dropbear-ws.py.txt
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/Farukbrowser/frk/main/insshws/ws-stunnel.txt
+wget -O /usr/local/bin/edu-proxyovpn https://raw.githubusercontent.com/Farukbrowser/frk/main/insshws/ovpn.py && chmod +x /usr/local/bin/edu-proxyovpn
 
 #izin permision
 chmod +x /usr/local/bin/edu-proxy
@@ -18,15 +18,15 @@ chmod +x /usr/local/bin/edu-proxyovpn
 
 
 #System Direcly dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/edu-proxy.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/http.service && chmod +x /etc/systemd/system/edu-proxy.service
+wget -O /etc/systemd/system/edu-proxy.service https://raw.githubusercontent.com/Farukbrowser/frk/main/insshws/http.service && chmod +x /etc/systemd/system/edu-proxy.service
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/Farukbrowser/frk/main/Insshws/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/Farukbrowser/frk/main/insshws/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/Farukbrowser/frk/main/Insshws/ws-stunnel.service.txt && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/Farukbrowser/frk/main/insshws/ws-stunnel.service.txt && chmod +x /etc/systemd/system/ws-stunnel.service
 
 ##System Websocket-OpenVPN Python
-wget -O /etc/systemd/system/edu-proxyovpn.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/ovpn.service && chmod +x /etc/systemd/system/edu-proxyovpn.service
+wget -O /etc/systemd/system/edu-proxyovpn.service https://raw.githubusercontent.com/Farukbrowser/frk/main/insshws/ovpn.service && chmod +x /etc/systemd/system/edu-proxyovpn.service
 
 #restart service
 #
