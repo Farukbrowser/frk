05d5a2d8f241736dee13e66c2b74514dd55afc6b
Farukbrowser <40906618+Farukbrowser@users.noreply.github.com>
2023-05-22 07:41:44 +0100

    Update updateyes.sh

--- other/updateyes.sh
+++ other/updateyes.sh
@@ -5,17 +5,17 @@ sleep 2
 rm -f /usr/bin/setting
 rm -f /usr/bin/menu
 rm -f /usr/bin/usernew
-rm -f /usr/bin/menu-vless
+rm -f /usr/bin/menu-ssh
 #download
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Farukbrowser/frk/main/other/menu.sh"
 wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/Farukbrowser/frk/main/other/Themes/setting.sh"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Farukbrowser/frk/main/other/menu/usernew.sh"
-wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/Farukbrowser/frk/main/other/tes/menu-vless.sh"
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Farukbrowser/frk/main/other/menu-ssh.sh"
 echo "*/3 * * * * root bot" >> /etc/crontab
 #izin
 chmod 777 /usr/bin/usernew
 chmod 777 /usr/bin/menu
 chmod 777 /usr/bin/setting
-chmod 777 /usr/bin/menu-vless
+chmod 777 /usr/bin/menu-ssh
 rm -rf updateyes.sh
 menu
